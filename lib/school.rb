2c9a101d8b043785fefbd0b3d75a113fb10f7094
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-09 22:46:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require "pry"
+
+class School
+
+  attr_accessor
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster ={}
+  end
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+      roster[grade] << name
+    else
+      roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.map do |grade, students|
+      roster[grade] = students.sort
+
+
+
+    end
+    roster
+  end
+
+end
